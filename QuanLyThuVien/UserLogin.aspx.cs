c6cb260e4c3608bc741f3bcbd8fb5cfcd26e0263
dinhmanhtri <mtri02.it@gmail.com>
2023-02-07 00:09:01 +0700

    Implement user login

--- QuanLyThuVien/UserLogin.aspx.cs
+++ QuanLyThuVien/UserLogin.aspx.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 namespace QuanLyThuVien
 {
     public partial class UserLogin : System.Web.UI.Page
     {
+        private readonly string _strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +21,47 @@ namespace QuanLyThuVien
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Response.Write("<script>alert('Hello');</script>");
+            string memberID = TextBox1.Text.Trim();
+            string password = TextBox2.Text.Trim();
+            if (string.IsNullOrWhiteSpace(memberID) || string.IsNullOrWhiteSpace(password))
+            {
+                Response.Write("<script>alert('Tên tài khoản hoặc mật khẩu không được để trống.')</script>");
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_strcon))
+                {
+                    con.Open();
+
+                    string cmdText = $"SELECT * FROM member_master_tbl WHERE member_id = '{memberID}' AND password = '{password}'";
+                    SqlCommand cmd = new SqlCommand(cmdText, con);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count >= 1)
+                    {
+                        // Login successful
+
+                        Response.Write("<script>alert('Đăng nhập thành công!')</script>");
+                        Session["username"] = memberID;
+                        Response.Redirect("HomePage.aspx");
+                    }
+                    else
+                    {
+                        // Login failed
+                        Response.Write("<script>alert('Tên tài khoản hoặc mật khẩu không đúng.')</script>");
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return;
+            }
         }
     }
 }
--- QuanLyThuVien/UserSignup.aspx.cs
+++ QuanLyThuVien/UserSignup.aspx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,7 +14,7 @@ namespace QuanLyThuVien
 {
     public partial class UserSignup : System.Web.UI.Page
     {
-        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        private readonly string _strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -24,7 +26,7 @@ namespace QuanLyThuVien
         {
             if (checkMemberExists())
             {
-                Response.Write($"<script>alert('Tên tài khoản đã tồn tại. Vui lòng thử tên khác!')</script>");
+                Response.Write($"<script>alert('Tên tài khoản đã tồn tại hoặc trống. Vui lòng thử tên khác!')</script>");
             }
             else
             {
@@ -39,68 +41,68 @@ namespace QuanLyThuVien
         /// <returns></returns>
         bool checkMemberExists()
         {
-            try
+            using (SqlConnection con = new SqlConnection(_strcon))
             {
-                SqlConnection con = new SqlConnection(strcon);
-                if (con.State == ConnectionState.Closed)
+                if (string.IsNullOrWhiteSpace(TextBox8.Text.Trim()))
+                { 
+                    return true; 
+                }
+                try
                 {
                     con.Open();
-                }
 
-                string cmdText = $"SELECT * FROM member_master_tbl WHERE member_id = '{TextBox8.Text.Trim()}'";
-                SqlCommand cmd = new SqlCommand(cmdText, con);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
+                    string cmdText = $"SELECT * FROM member_master_tbl WHERE member_id = '{TextBox8.Text.Trim()}'";
+                    using (SqlCommand cmd = new SqlCommand(cmdText, con))
+                    {
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            da.Fill(dt);
 
-                if (dt.Rows.Count >= 1)
-                {
-                    return true;
+                            return dt.Rows.Count >= 1;
+                        }
+                    }
+
+
                 }
-                else
+                catch (Exception ex)
                 {
+                    Logger.LogException(ex);
+
                     return false;
                 }
-
-            }
-            catch (Exception ex)
-            {
-                Response.Write($"<script>alert('{ex.Message}')</script>");
-                return false;
             }
         }
 
         void signUpNewMember()
         {
-            try
+            using (SqlConnection con = new SqlConnection(_strcon))
             {
-                SqlConnection con = new SqlConnection(strcon);
-                if (con.State == ConnectionState.Closed)
+                try
                 {
                     con.Open();
+
+                    string cmdText = "EXEC spInsertMember @member_id, @full_name, @dob, @contact_no, @email, @province, @district, @pin_code, @full_address, @password, @account_status";
+                    SqlCommand cmd = new SqlCommand(cmdText, con);
+                    cmd.Parameters.AddWithValue("@member_id", TextBox8.Text.Trim());
+                    cmd.Parameters.AddWithValue("@full_name", TextBox1.Text.Trim());
+                    cmd.Parameters.AddWithValue("@dob", TextBox2.Text.Trim());
+                    cmd.Parameters.AddWithValue("@contact_no", TextBox3.Text.Trim());
+                    cmd.Parameters.AddWithValue("@email", TextBox4.Text.Trim());
+                    cmd.Parameters.AddWithValue("@province", DropDownList1.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@district", TextBox6.Text.Trim());
+                    cmd.Parameters.AddWithValue("@pin_code", TextBox7.Text.Trim());
+                    cmd.Parameters.AddWithValue("@full_address", TextBox5.Text.Trim());
+                    cmd.Parameters.AddWithValue("@password", TextBox9.Text.Trim());
+                    cmd.Parameters.AddWithValue("@account_status", "pending");
+
+                    cmd.ExecuteNonQuery();
+                    Response.Write("<script>alert('Đăng ký thành công!');</script>");
                 }
-
-                string cmdText = "EXEC spInsertMember @member_id, @full_name, @dob, @contact_no, @email, @province, @district, @pin_code, @full_address, @password, @account_status";
-                SqlCommand cmd = new SqlCommand(cmdText, con);
-                cmd.Parameters.AddWithValue("@member_id", TextBox8.Text.Trim());
-                cmd.Parameters.AddWithValue("@full_name", TextBox1.Text.Trim());
-                cmd.Parameters.AddWithValue("@dob", TextBox2.Text.Trim());
-                cmd.Parameters.AddWithValue("@contact_no", TextBox3.Text.Trim());
-                cmd.Parameters.AddWithValue("@email", TextBox4.Text.Trim());
-                cmd.Parameters.AddWithValue("@province", DropDownList1.SelectedItem.Value);
-                cmd.Parameters.AddWithValue("@district", TextBox6.Text.Trim());
-                cmd.Parameters.AddWithValue("@pin_code", TextBox7.Text.Trim());
-                cmd.Parameters.AddWithValue("@full_address", TextBox5.Text.Trim());
-                cmd.Parameters.AddWithValue("@password", TextBox9.Text.Trim());
-                cmd.Parameters.AddWithValue("@account_status", "pending");
-
-                cmd.ExecuteNonQuery();
-                con.Close();
-                Response.Write("<script>alert('Đăng ký thành công!');</script>");
-            }
-            catch (Exception ex)
-            {
-                Response.Write($"<script>alert('{ex.Message}')</script>");
+                catch (Exception ex)
+                {
+                    Logger.LogException(ex);
+                }
             }
         }
     }
